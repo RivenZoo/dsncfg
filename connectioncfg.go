074ae2d74302b8ff3cf18e42fe245b7e853ee081
zrw <zrw2010@gmail.com>
2019-01-17 10:49:10 +0800

    feat: add connection config

---
+++ connectioncfg.go
@@ -0,0 +1,7 @@
+package dsncfg
+
+type ConnectionConfig struct {
+	MaxOpenConnections int `json:"max_open_connections"`
+	MaxIdleConnections int `json:"max_idle_connections"`
+	MaxLifeTime        int `json:"max_life_time"`
+}
