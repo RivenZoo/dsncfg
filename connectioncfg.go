3f8fd4027e519bb6cd1522da95e6d49461b21a76
zrw <zrw2010@gmail.com>
2019-01-17 11:08:00 +0800

    docs: add comment

--- connectioncfg.go
+++ connectioncfg.go
@@ -3,5 +3,5 @@ package dsncfg
 type ConnectionConfig struct {
 	MaxOpenConnections int `json:"max_open_connections"`
 	MaxIdleConnections int `json:"max_idle_connections"`
-	MaxLifeTime        int `json:"max_life_time"`
+	MaxLifeTime        int `json:"max_life_time"` // max connection life time seconds
 }
